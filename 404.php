a08f6322cb1b64c699ede4d3d7383701e868fcba
Bogdan <47315165+gbs99@users.noreply.github.com>
2019-09-27 13:10:37 +0400

    Delete 404.php

--- 404.php
+++
@@ -1,4 +0,0 @@
-
-<div class="404" style="background-image: url(<?php getStaticFilesUrl() ?>img/404img.jpg);width:100%;height:100%;background-size:contain;background-position:center;background-repeat:no-repeat;">
-
-</div>
